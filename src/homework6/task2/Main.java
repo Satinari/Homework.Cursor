e6ee44f414b3db9210c52fa76234af6ccd1fd9f3
Satinari <izzey5960@gmail.com>
2021-04-09 07:52:21 +0300

    Commit. Homework 6.

--- src/homework6/task2/Main.java
+++ src/homework6/task2/Main.java
@@ -1,22 +1,22 @@
-package homework6.task2;
-
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Reader reader = new Reader();
-        System.out.println(reader.readerFromUrl());
-//        Path file = Path.of("src\\homework6\\task2\\text");
-//        List<String> list = Files.readAllLines(file);
+//package homework6.task2;
 //
-//        for (String str : list)
-//            System.out.println(str);
-
-//        try(InputStream input = connection.getInputStream();
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(input)))
-//        {
-//            while (reader.ready())
-//                System.out.println(reader.readLine());
-//        }
-    }
-}
+//import java.io.IOException;
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        Reader reader = new Reader();
+//        System.out.println(reader.readerFromUrl());
+////        Path file = Path.of("src\\homework6\\task2\\text");
+////        List<String> list = Files.readAllLines(file);
+////
+////        for (String str : list)
+////            System.out.println(str);
+//
+////        try(InputStream input = connection.getInputStream();
+////            BufferedReader reader = new BufferedReader(new InputStreamReader(input)))
+////        {
+////            while (reader.ready())
+////                System.out.println(reader.readLine());
+////        }
+//    }
+//}
