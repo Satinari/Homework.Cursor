2ca18a4980959f8fc6513fe65cc3b87e19fff947
Satinari <izzey5960@gmail.com>
2021-04-09 07:51:19 +0300

    Commit. Homework 6.

--- src/homework6/task2/Main.java
+++ src/homework6/task2/Main.java
@@ -1,7 +1,22 @@
 package homework6.task2;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Reader reader = new Reader();
+        System.out.println(reader.readerFromUrl());
+//        Path file = Path.of("src\\homework6\\task2\\text");
+//        List<String> list = Files.readAllLines(file);
+//
+//        for (String str : list)
+//            System.out.println(str);
+
+//        try(InputStream input = connection.getInputStream();
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(input)))
+//        {
+//            while (reader.ready())
+//                System.out.println(reader.readLine());
+//        }
     }
 }
---
+++ src/homework6/task2/Reader.java
@@ -0,0 +1,46 @@
+package homework6.task2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Reader {
+    public String readerFromUrl() throws IOException {
+
+        String src = "src\\homework6\\task2\\text";
+        HashMap<Character, Integer> setOfWords = new HashMap<>();
+        int counter;
+        src = src.toLowerCase();
+
+        try(FileReader in = new FileReader(src);
+            BufferedReader reader = new BufferedReader(in))
+        {
+            while (reader.ready())
+            {
+                for (int i = 0; i < src.length(); i++) {
+                    counter = 0;
+                    for (int j = 0; j < src.length(); j++) {
+                        if (src.charAt(i) == src.charAt(j)) {
+                            counter++;
+                        }
+                        setOfWords.put(src.charAt(i), counter);
+                    }
+                }
+
+                for (Map.Entry entry : setOfWords.entrySet()) {
+                    setOfWords.values().stream().min(Integer::compareTo);
+                    System.out.println("Lowest word is: " + entry);
+                }
+
+                for (Map.Entry entry : setOfWords.entrySet()) {
+                    setOfWords.values().stream().max(Integer::compareTo);
+                    System.out.println("bigger word is: " + entry);
+                }
+            }
+        }
+
+        return "";
+    }
+}
