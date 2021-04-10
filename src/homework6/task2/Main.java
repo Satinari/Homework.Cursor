4fc51d7bba61081470a93c815f6a79c3a2a2703d
Satinari <izzey5960@gmail.com>
2021-04-10 12:27:24 +0300

    Commit. Homework 6. rewrite task2

--- src/homework6/task2/Main.java
+++ src/homework6/task2/Main.java
@@ -1,22 +1,10 @@
-//package homework6.task2;
-//
-//import java.io.IOException;
-//
-//public class Main {
-//    public static void main(String[] args) throws IOException {
-//        Reader reader = new Reader();
-//        System.out.println(reader.readerFromUrl());
-////        Path file = Path.of("src\\homework6\\task2\\text");
-////        List<String> list = Files.readAllLines(file);
-////
-////        for (String str : list)
-////            System.out.println(str);
-//
-////        try(InputStream input = connection.getInputStream();
-////            BufferedReader reader = new BufferedReader(new InputStreamReader(input)))
-////        {
-////            while (reader.ready())
-////                System.out.println(reader.readLine());
-////        }
-//    }
-//}
+package homework6.task2;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Reader reader = new Reader();
+        reader.readerFromUrl();
+    }
+}
--- src/homework6/task2/Reader.java
+++ src/homework6/task2/Reader.java
@@ -1,46 +1,21 @@
-//package homework6.task2;
-//
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class Reader {
-//    public String readerFromUrl() throws IOException {
-//
-//        String src = "src\\homework6\\task2\\text";
-//        HashMap<Character, Integer> setOfWords = new HashMap<>();
-//        int counter;
-//        src = src.toLowerCase();
-//
-//        try(FileReader in = new FileReader(src);
-//            BufferedReader reader = new BufferedReader(in))
-//        {
-//            while (reader.ready())
-//            {
-//                for (int i = 0; i < src.length(); i++) {
-//                    counter = 0;
-//                    for (int j = 0; j < src.length(); j++) {
-//                        if (src.charAt(i) == src.charAt(j)) {
-//                            counter++;
-//                        }
-//                        setOfWords.put(src.charAt(i), counter);
-//                    }
-//                }
-//
-//                for (Map.Entry entry : setOfWords.entrySet()) {
-//                    setOfWords.values().stream().min(Integer::compareTo);
-//                    System.out.println("Lowest word is: " + entry);
-//                }
-//
-//                for (Map.Entry entry : setOfWords.entrySet()) {
-//                    setOfWords.values().stream().max(Integer::compareTo);
-//                    System.out.println("bigger word is: " + entry);
-//                }
-//            }
-//        }
-//
-//        return "";
-//    }
-//}
+package homework6.task2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader {
+    public void readerFromUrl() throws IOException {
+        String src = "src\\homework6\\task2\\text";
+        File file = new File(src);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while((line = br.readLine()) != null){
+            System.out.println(line);
+        }
+        br.close();
+        fr.close();
+    }
+}
