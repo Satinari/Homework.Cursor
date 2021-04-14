7ce55f80b925a84f648e0792a83150b8c33cce86
Satinari <izzey5960@gmail.com>
2021-04-14 07:48:54 +0300

    Commit. Homework 6.

--- src/homework6/task2/Main.java
+++ src/homework6/task2/Main.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Reader reader = new Reader();
-        reader.readerFromUrl();
+        Reader reader = new Reader("src\\homework6\\task2\\text");
+        reader.readerFromFile();
+
+        System.out.println("Short word is: " + reader.shortWords() + ", number of word in the text: " + reader.getValueHashMap(reader.shortWords()));
+        System.out.println("Short word is: " + reader.longWords() + ", number of word in the text: " + reader.getValueHashMap(reader.longWords()));
     }
 }
--- src/homework6/task2/Reader.java
+++ src/homework6/task2/Reader.java
@@ -5,21 +5,70 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Reader {
-    public void readerFromUrl() throws IOException {
-        String src = "src\\homework6\\task2\\text";
+    String src;
+    int minWord = 3;
+    private final HashMap<String, Integer> words = new HashMap<>();
+
+    public Reader(String src) {
+        this.src = src;
+    }
+
+    public void readerFromFile() throws IOException {
         File file = new File(src);
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
         String line;
         while((line = br.readLine()) != null){
-            System.out.println(line);
+            String word = src.replaceAll("[^а-яёА-ЯЁ]", "");
+            if (words.containsKey(word)) {
+                int valueInt = words.get(word) + 1;
+                words.put(word, valueInt);
+            } else {
+                words.put(word, 1);
+            }
         }
         br.close();
         fr.close();
+    }
 
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        
+    public String longWords() {
+        String longWord = "";
+        for (Map.Entry entry : words.entrySet()) {
+            String entryKey = entry.getKey().toString();
+            if (entryKey.length() >= minWord && longWord.length() <= entryKey.length()) {
+                if (longWord.length() == entryKey.length()) {
+                    if (longWord.compareTo(entryKey) <= 0) {
+                        longWord = entryKey;
+                    }
+                } else {
+                    longWord = entryKey.toString();
+                }
+            }
+        }
+        return longWord;
+    }
+
+    public String shortWords() {
+        String shortWord = longWords();
+        for (Map.Entry entry : words.entrySet()) {
+            String entryKey = entry.getKey().toString();
+            if (entryKey.length() >= minWord && shortWord.length() >= entryKey.length()) {
+                if (shortWord.length() == entryKey.length()) {
+                    if (shortWord.compareTo(entryKey) >= 0) {
+                        shortWord = entryKey;
+                    }
+                } else {
+                    shortWord = entryKey.toString();
+                }
+            }
+        }
+        return shortWord;
+    }
+
+    public Integer getValueHashMap(String string) {
+        return words.get(string);
     }
 }
